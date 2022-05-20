e0e1947a06487173e8410987823cdcc8e30d900f
Dax2976 <98029197+Dax2976@users.noreply.github.com>
2022-05-20 12:43:15 +0200

    struttura dati

---
+++ snack_1/index.php
@@ -0,0 +1,58 @@
+
+    
+    
+<!-- Creiamo un array contenente le partite di basket 
+di un’ipotetica tappa del calendario. Ogni array avrà una squadra 
+di casa e una squadra ospite, punti fatti dalla squadra di casa e punti 
+fatti dalla squadra ospite. Stampiamo a schermo 
+tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60 -->
+
+
+
+
+<?php
+$matches = [
+    [
+        'team1' => 'los angeles lakers',
+        'team2' => 'detroit pistons',
+        'team1_point' => '82',
+        'team2_point' => '113',
+    ],
+    [
+        'team1' => 'chicago bulls',
+        'team2' => 'bucks',
+        'team1_point' => '98',
+        'team2_point' => '102',
+    ],
+    [
+        'team1' => 'golden state wariors',
+        'team2' => 'orlando magic',
+        'team1_point' => '112',
+        'team2_point' => '111',
+    ],
+];
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Snack1</title>
+</head>
+<body>
+    <?php
+       
+
+
+
+
+
+    ?>
+</body>
+</html>
